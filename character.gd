d39feb3df2a9c4278078d34865d2a42fe447d696
carter <carteralfers@gmail.com>
2026-09-19 21:46:48 -0500

    sample procedual generation with art overhaul

--- character.gd
+++ character.gd
@@ -5,7 +5,12 @@ const SPEED = 280.00
 const JUMP_VELOCITY = -400.0
 const ACCELERATION = 1500
 const DECCELERATION = 2000
-
+const DASH_SPEED=400
+var dash := false
+var dash_direction
+var direction
+var dash_timer = 15
+var dash_cooldown=0
 
 func _physics_process(delta: float) -> void:
 	
@@ -15,15 +20,36 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_just_pressed("dash") and dash == false and dash_cooldown == 0:
+		dash=true
+		dash_direction=direction
+		$dashindicator.visible=true
 
 	
-	var direction := Input.get_axis("move_left","move_right")
+	direction = Input.get_axis("move_left","move_right")
+	
+	if dash_cooldown!=0:
+		dash_cooldown-=1
 	
 	if direction !=0:
 		velocity.x = move_toward(velocity.x, SPEED*direction, ACCELERATION*delta)
 	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		
+	if dash:
+		velocity.x = DASH_SPEED*dash_direction
+		dash_timer-=1
+		
+	if dash_timer==0:
+		dash=false
+		dash_timer=20
+		dash_cooldown=150
+		$dashindicator.visible=false
+		
+		
 	
 
 	move_and_slide()
--- main.gd
+++ main.gd
@@ -5,14 +5,34 @@ var mode: String = "negative"
 @onready var pickaxe: Node2D = $character/miningpivot/pickaxe
 const MINING_PARTICLES = preload("res://mineparticle.tscn")
 
+const SOURCE_ID = 0
+const GRASS = Vector2i(1,1)
+const DIRT = Vector2i(0,0)
+const STONE = Vector2i(1,0)
+const CRYSTAL = Vector2i(0,1)
+
+var cave_noise := FastNoiseLite.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	cave_noise.noise_type=FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+	cave_noise.seed=randi()
+	cave_noise.frequency=0.055
+	
+	cave_noise.fractal_type = FastNoiseLite.FRACTAL_FBM
+	cave_noise.fractal_octaves = 4
+	cave_noise.fractal_lacunarity = 2.0
+	cave_noise.fractal_gain = 0.5
+	
+	generate_world()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pickaxe.visible = false
+	
+	if Input.is_action_just_pressed("surface"):
+		_on_ui_surface()
 	
 	if Input.is_action_pressed("mine_toggle"):
 		pickaxe.visible = true
@@ -68,9 +88,41 @@ func activate_particles(cords) -> void:
 func camera_shake() -> void:
 	print("shook")
 	for i in range(5):
-		var shake_vector=Vector2(randf_range(-0.45,0.45),randf_range(-0.45,0.45))
+		var shake_vector=Vector2(randf_range(-0.55,0.55),randf_range(-0.55,0.55))
 		$character/Camera2D.position+=shake_vector
 	
 		
 func _on_ui_surface() -> void:
-	$character.position.y=140
+	$character.global_position.y=-20
+	
+func generate_world() -> void:
+	for x in range(600):
+		for y in range(150):
+			var cell
+			var randomn := randf()
+			var cave_noise_value = cave_noise.get_noise_2d(x,y)
+			
+			if cave_noise_value < -0.25:
+				continue
+				
+			else:
+				if randomn > 0.98:
+					cell=CRYSTAL
+				
+				#elif randomn <=0.98 and randomn >0.7:
+					#cell=GRASS
+					
+				elif randomn <=0.98 and randomn >0.4:
+					cell=DIRT
+					
+				elif randomn <=0.4:
+					cell=STONE
+					
+			
+			
+			
+			terrain.set_cell(
+				Vector2i(x,y),
+				SOURCE_ID,
+				cell
+			)
